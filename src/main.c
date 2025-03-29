aee1e8a71f5a2cceaa4f516055e343522df67d36
Victor <vibarrientosp@gmail.com>
2025-03-29 15:32:56 -0500

    Add initial structure for command handling

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <assert.h>
 
+#include "args.h"
+
+#define DEFAULT_PREFIX "!"
+
 void
 on_ready(struct discord* client, const struct discord_ready* event)
 {
@@ -16,13 +20,34 @@ on_ready(struct discord* client, const struct discord_ready* event)
 void
 on_message_create(struct discord* client, const struct discord_message* event)
 {
+    if (event->content[0] != '!') return;
+
+    struct message_arguments *message_args = get_message_arguments(event->content);
+    char *command_name = strtok(message_args->arguments[0], DEFAULT_PREFIX);
+
+    if (command_name == NULL) command_name = DEFAULT_PREFIX;
+
+    if (strcmp(command_name, "ping") == 0)
+    {
+        struct discord_create_message params = {
+            .content="Pong!"
+        };
+        discord_create_message(client, event->channel_id, &params, NULL);
+    } else
+    {
+        struct discord_create_message msg = {
+            .content="This name doesn't belong to none available command."
+        };
+        discord_create_message(client, event->channel_id, &msg, NULL);
+    }
     
+    free_message_arguments_structure(message_args);
 }
 
 int main(void) {
     ccord_global_init();
 
-    struct discord* client = discord_config_init("../config.json");
+    struct discord* client = discord_config_init("config.json");
     assert(NULL != client && "Couldn't initialize client");
 
     discord_add_intents(client, DISCORD_GATEWAY_MESSAGE_CONTENT);
